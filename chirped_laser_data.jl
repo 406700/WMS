71ad1c8bd4e90316cbcb49141630ecc78eb3b9b9
406700km <michael.aaron@zoho.eu>
2022-07-07 10:05:25 +0200

    struc

--- chirped_laser_data.jl
+++ chirped_laser_data.jl
@@ -1,1 +1,15 @@
-chirped_laser_data
+struct Experiment
+    OSA_file::String
+    tfbg_file::String
+    system_risetime_file::String
+    λ0::Float64
+    ΔI::Float64
+end
+
+exp_27_281_16=Experiment(
+    "./Data/OSA_data/TFBG1_4deg_20220705.csv",
+    "./Data/20220706_varied_measurements/27C 2kHz 16 281mA_ALL.csv",
+    "./Data/20220706_varied_measurements/27C 2kHz 16 281mA POWER.csv",
+    1547.439,
+    12.1,
+)
