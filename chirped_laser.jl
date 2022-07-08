e817de25a862b09ebda143479ac20dff4d712301
406700km <michael.aaron@zoho.eu>
2022-07-08 13:57:23 +0200

    some changes

--- chirped_laser.jl
+++ chirped_laser.jl
@@ -4,7 +4,7 @@ using DelimitedFiles
 using LsqFit
 using Interpolations
 plotlyjs()
-
+include("chirped_laser_data.jl")
 #find chirp wavelength as a  function of t, by matching the loss to the known grating profile
 
 function calibrate_wavelength(grating_wavelength,grating_spectrum,total_loss,initial_wavelength_index)
@@ -12,31 +12,26 @@ function calibrate_wavelength(grating_wavelength,grating_spectrum,total_loss,ini
     current_index=initial_wavelength_index
     for i in 1:length(total_loss)
         current_index=find_wavelength(total_loss[i],grating_spectrum,current_index)
-        try
-            λ_of_t[i]=grating_wavelength[current_index]
-        catch
-        end
+        λ_of_t[i]=grating_wavelength[current_index]
     end
     return λ_of_t
 end
-
 
 #compares the attenuation of the single passed through the TFBG to that of the known grating spectrum. The resolution should be comparable to the noise and should ensure a steady increase in wavelength
 #The function avoids jumps in wavelength by searching around the last wavelength value.
 
 function find_wavelength(total_loss, grating_spectrum, current_index)
-    resolution = 0.001 # related to the wavelength resolution variable in TFBG_spectrum_setup
+    #resolution = 0.05 # related to the wavelength resolution variable in TFBG_spectrum_setup
     for i = 1:length(grating_spectrum[current_index-200:end])# NB subtration starts a little below the currunt wavelength value in case of noise or reverse in direction. Will break if spectrum starts near the initial wavelength.
         if abs(grating_spectrum[i] - total_loss) < resolution
             return i
         end
     end
-    error("no matching loss found, check if the initial wavelength is correct.")
+    error("measured loss and spectrum loss do not match. Check resolution, λ0, or consider if grating has shifted.")
 end
 
 #osc_file= photodetector output with chirp passing through grating.
 #spec file, is the grating data from the OSA
-
 
 function initialize(experiment)
     # even with same oscilloscope settings trigger may not occur at exactly the same place.
@@ -45,7 +40,7 @@ function initialize(experiment)
     tfbg_file=experiment.tfbg_file
     system_risetime_file=experiment.system_risetime_file
     λ0=experiment.λ0
-    
+
     pulselength=Int(5e5)#1.04
     data=readdlm(tfbg_file,',',skipstart=12)#time,channel1, channel,2volts seconds
     time_axis=data[:,1]*1e6 #convert to us
@@ -59,22 +54,23 @@ function initialize(experiment)
     time_axis=time_axis[t_0:t_0+pulselength] #set pulse length
     volt_nograting=data[t_0:t_0+pulselength,2] #index 2 due to way files are saved
 
+    #account for the different losses between the two spectrum. methodology, not code, may need review.
     v0_grating=mean(volt_grating[1:1000])
     v0_nograting=mean(volt_nograting[1:1000])
     connectorloss=v0_grating/v0_nograting
     volt_nograting=volt_nograting.*connectorloss
-    #gui(plot(time_axis,hcat(volt_nograting,volt_grating)))
+    gui(plot(time_axis,hcat(volt_nograting,volt_grating)))
     total_loss=volt_grating./volt_nograting
-    #gui(plot(total_loss))
-    grating_wavelength,grating_spectrum,initial_wavelength_index=TFBG_spectrum_setup(λ0,)
-    #gui(plot(grating_wavelength,grating_spectrum))
+    gui(plot(total_loss))
+    grating_wavelength,grating_spectrum,initial_wavelength_index=TFBG_spectrum_setup(λ0,osa_file)
+    gui(plot(grating_wavelength,grating_spectrum))
     result=calibrate_wavelength(grating_wavelength,grating_spectrum,total_loss,initial_wavelength_index)
+
     return result,time_axis
 end
 
 function TFBG_spectrum_setup(λ0,filename)
     #read the grating spectrum, cut it to 2 nm around the starting wavelength, normalize the percentage of loss to the starting wavelength, interpolate the results and return new vectors with 1pm resolution.
-
     data_OSA=readdlm(filename,',',skipstart=79)[1:end-1,:]#,header=true)[1]
     grating_wavelength=data_OSA[:,1] #shorten grating spectrum to include a few peaks around λ0
 
@@ -83,23 +79,20 @@ function TFBG_spectrum_setup(λ0,filename)
     spectrum_stop_index=findfirst(x->x>(λ0+spectrum_width/2),grating_wavelength)
 
     grating_wavelength=grating_wavelength[spectrum_start_index:spectrum_stop_index]
-    initial_wavelength_index=findfirst(x->x>λ0,grating_wavelength) #find index of λ0
-
-    #normalize the grating spectrum to the starting wavelength
-    grating_spectrum=data_OSA[spectrum_start_index:spectrum_stop_index,2]
-    grating_spectrum=grating_spectrum/grating_spectrum[initial_wavelength_index] #normalize
-
-    #interpolate the TFBG spectrum and create new vectors with 1 pm resolution
+    grating_spectrum=data_OSA[:,2][spectrum_start_index:spectrum_stop_index]
+    #gui(plot(grating_wavelength,grating_spectrum))
+    #interpolate the TFBG spectrum and create new vectors with 1 pm resolution. Find the interpolated value at the exact starting wavelength for normalization
     grating_spectrum_itp=Interpolations.LinearInterpolation(grating_wavelength,grating_spectrum) #interpolate spectrum. not sure linear is correct.
-    resolution=1e-5 #wavelength step size in nm
-    wavelength_range=range(grating_wavelength[1],stop=grating_wavelength[end],step=resolution) #new wavelength vector
-    grating_spectrum=grating_spectrum_itp.(wavelength_range)
-    initial_wavelength_index_itp=findfirst(x->x>λ0,wavelength_range) #get the new starting index for λ0
+    normalization_constant=grating_spectrum_itp(λ0)
+    #spectral_resolution=5e-5 #wavelength step size in nm
+    wavelength_range=range(grating_wavelength[1],stop=grating_wavelength[end],step=spectral_resolution) #new wavelength vector
+    grating_spectrum=grating_spectrum_itp.(wavelength_range)  #new spectrum vector from wavelength range
+    grating_spectrum=grating_spectrum/normalization_constant
 
+    #get the new starting index for λ0
+    initial_wavelength_index_itp=findfirst(x->x>λ0,wavelength_range)
     return wavelength_range,grating_spectrum,initial_wavelength_index_itp
-
 end
-
 
 function thermal_model(t)
     #us, nm,
@@ -107,7 +100,8 @@ function thermal_model(t)
     λ0=1546.653
     p=[]
     λ0 + ΔI*(p[1]+p[2]*(1 -exp(-t / p[5])) + p[3] * (1 - exp(-t / p[6])) + p[4] * t)
-en
+end
+
 function perform_fit(ΔI, λ0, chirp, time_axis)
     #t=time_axis.*1e3#convert to microseconds
 
@@ -125,17 +119,24 @@ function perform_fit(ΔI, λ0, chirp, time_axis)
     return fit
 end
 
+global const resolution=.05 # difference between grating loss and measured loss, that are considered a match. should be related to the measurement noise.
+global const spectral_resolution=5e-3 #wavelength spacing of the interpolated grating profile
 
-#global const λ0 = 1548.22
-global const ΔI = 12.1
+p=plot()
+for i in 3:3#3:length(ChirpedLaserData.experiment_list)
+    experiment=ChirpedLaserData.experiment_list[i]
+    chirp,time_axis=initialize(experiment)
+    plot!(p,time_axis,chirp, xlabel="time (ms)",ylabel="wavelength (nm)",series="$i")
+end
+plot(p)
 
-chirp,time_axis=initialize(1547.439)
-plot(time_axis,chirp, xlabel="time (ms)",ylabel="wavelength (nm)")
-chirp_length=Int(5e4)
-fit=perform_fit(ΔI,1546.652,chirp[1:chirp_length],time_axis[1:chirp_length])
-plot!(time_axis[1:chirp_length],thermal_model.(time_axis[1:chirp_length]))
-plot!(time_axis[1:chirp_length],chirp_pm[1:chirp_length])
-# fit=perform_fit(ΔI,λ0,chirp,time_axis)
+
+#fit=perform_fit(ΔI,0,(chirp[1:chirp_length].-1546.6),time_axis[1:chirp_length])
+# plot!(time_axis[1:chirp_length],thermal_model.(time_axis[1:chirp_length]))
+# plot!(time_axis[1:chirp_length],chirp_pm[1:chirp_length])
+# # fit=perform_fit(ΔI,λ0,chirp,time_axis)
 # p=fit.param
 # plot!(time_axis,thermal_model.(time_axis))
 # plot!(time_axis,chirp)
+
+#writedlm("./Results/chirp_27_16_281.csv",hcat(time_axis,chirp),',')
--- chirped_laser_data.jl
+++ chirped_laser_data.jl
@@ -1,15 +1,102 @@
+module ChirpedLaserData
+export experiment_list
+
 struct Experiment
     OSA_file::String
     tfbg_file::String
     system_risetime_file::String
     λ0::Float64
+    λ_high::Float64
     ΔI::Float64
+    amplitude::Float64
+    offset::Float64
+    temp::Float64
+    i_low::Float64
+    i_high::Float64
 end
 
-exp_27_281_16=Experiment(
+
+exp_372_60=Experiment(
+    "./Data/OSA_data/TFBG1_4deg_20220705.csv",
+    "./Data/20220706_varied_measurements/372C 2kHz 13_313mA_ALL.csv",
+    "./Data/20220706_varied_measurements/372C 2kHz 13 313mA POWER.csv",
+    1548.252,
+    1548.451,
+    18.3,
+    60,
+    30,
+    37.2,
+    13,
+    31.3,
+)
+
+exp_273_60=Experiment(
+    "./Data/OSA_data/TFBG1_4deg_20220705.csv",
+    "./Data/20220706_varied_measurements/273C 2kHz 129 312mA_ALL.csv",
+    "./Data/20220706_varied_measurements/273C 2kHz 129 312mA POWER.csv",
+    1547.447,
+    1547.639,
+    18.3,
+    60,
+    30,
+    27.3,
+    12.9,
+    31.2
+)
+# exp_362_60=Experiment(
+#     "./Data/OSA_data/TFBG1_4deg_20220705.csv",
+#     "./Data/20220706_varied_measurements/372C 2kHz 13_313mA_ALL.csv",
+#     "./Data/20220706_varied_measurements/372C 2kHz 13 313mA POWER.csv",
+#     1548.252,
+#     1548.451,
+#     18.3,
+#     60,
+#     30,
+#     37.2,
+#     13,
+#     31.3,
+# )
+exp_27_40=Experiment(
     "./Data/OSA_data/TFBG1_4deg_20220705.csv",
     "./Data/20220706_varied_measurements/27C 2kHz 16 281mA_ALL.csv",
     "./Data/20220706_varied_measurements/27C 2kHz 16 281mA POWER.csv",
     1547.439,
+    1547.56,
     12.1,
+    40,
+    30,
+    27,
+    16,
+    28.1
 )
+
+exp_255_60_25C=Experiment(
+    "./Data/friday080722/tfbg_spectrum_20220708.csv",
+    "./Data/friday080722/255C TEC 25C TFBG_ALL.csv",
+    "./Data/friday080722/255C TEC 25C TFBG POWER_ALL.csv",
+    1547.279,
+    1547.474,
+    18.3,
+    60,
+    30,
+    25.5,
+    13.7,
+    32
+)
+
+exp_272_60_25C=Experiment(
+    "./Data/friday080722/tfbg_spectrum_20220708.csv",
+    "./Data/friday080722/272C TEC 25C TFBG_ALL.csv",
+    "./Data/friday080722/272C TEC 25C TFBG POWER_ALL.csv",
+    1547.419,
+    1547.614,
+    18.3,
+    60,
+    30,
+    27.2,
+    13.7,
+    32
+)
+experiment_list=[exp_372_60,exp_273_60,exp_27_40,exp_255_60_25C,exp_272_60_25C]
+
+end
