b3ad2526beb469c12bd1f882f89696d5831a3037
406700km <michael.aaron@zoho.eu>
2024-11-25 00:06:57 +0100

    working but incorrect

--- gas_experiment_main.jl
+++ gas_experiment_main.jl
@@ -36,10 +36,6 @@ function read_det_data(det_data_path,trig_level)
     return ref_chan,sig_chan
 end
 
-#problem: time varying amplitude and 
-function normalize_det_data(sig_chan,ref_chan)
-    # mean(ref_chan)
-end
 
 function divide_scans(sig_chan,ref_chan)
     return sig_chan[1:66000000],ref_chan[1:66000000]
@@ -54,23 +50,52 @@ function find_ref_trigger_level(ref_chan,downsample)
     return ref_trig_level_slope,ref_trig_level_intercept
 end
 
-function find_pulse_trig_points(ref_chan,ref_trig_level_intercept,ref_trig_level_slope)
-    half_periods_of_data = round(Int, length(ref_chan) / data_points_per_half_period)
-    trig_indices = Vector{Int}(undef, half_periods_of_data) 
-    for i in 1:half_periods_of_data
-        local_data=ref_chan[(i - 1) * data_points_per_half_period + 1:(i*data_points_per_half_period)]
-        local_trig_level=ref_trig_level_slope*i*data_points_per_half_period+ref_trig_level_intercept #not exact but error should be smaller than data spacing
-        trig_index=findmin(abs.(local_data.-local_trig_level))[2]
-        trig_tolerance=100
-        if abs(local_data[trig_index]-local_trig_level)>trig_tolerance
-            error("the difference in the trig level and trig point is greater than $trig_tolerance mv")
+function find_pulse_trig_points(ref_chan, ref_trig_level_intercept, ref_trig_level_slope,data_points_per_half_period)
+    trig_indices = Int[]
+    length_ref_chan = length(ref_chan)
+    trig_tolerance = 100  # Adjust as needed, for unnormalized data.
+
+    first_trig_index=findmin(abs.(ref_chan[1:data_points_per_half_period].-ref_trig_level_intercept))[2]
+    push!(trig_indices,first_trig_index)
+    # 2. Find subsequent trigger points by searching in a window centered ahead
+    window_size = 20  # Number of data points to search in each window
+    half_window = window_size/2
+
+    while true
+        # Expected index is data_points_per_half_period ahead of the last trigger point
+        expected_index = trig_indices[end] + data_points_per_half_period
+        if expected_index > length_ref_chan
+            break  # Exit if expected index exceeds data length
         end
-        trig_index=trig_index + (i - 1) * data_points_per_half_period #ignore the plus 1 due to the addition.
-        trig_indices[i]=trig_index
-     end 
+
+        # Define the search window centered around the expected index
+        window_start = Int(expected_index - half_window)
+        window_end = Int(min(expected_index + half_window - 1, length_ref_chan)) #keeps in bounds
+        local_indices = window_start:window_end
+        local_data = ref_chan[local_indices]
+
+        # Compute the local trigger level at the expected index
+        local_trig_level = ref_trig_level_slope * expected_index + ref_trig_level_intercept
+
+        # Find the index in the window where the data crosses the trigger level
+        differences = abs.(local_data .- local_trig_level)
+        min_difference, min_idx = findmin(differences)
+
+        if min_difference > trig_tolerance
+            error("Trigger point not found within tolerance at index $expected_index.")
+        end
+
+        # Get the actual index in ref_chan
+        trig_index = local_indices[min_idx]
+        push!(trig_indices, trig_index)
+    end
+    if maximum(diff(diff(trig_indices))) >5
+        error("finding trig indices: variation in period > 5 sample")
+    end
     return trig_indices
- 
 end
+
+
 
 function L0(i_p,i_m,Δim)
     1/(2*(1-Δim^2))*(i_p-Δim*i_m)
@@ -80,52 +105,44 @@ function L0_prime(i_p,i_m,Δν_h,Δim)
    1/(Δν_h*2*(1-Δim^2))*(i_m-Δim*i_p)
 end
 
-function average_values(local_data,rise_time,fall_time,trig_indices,index)
-    i_plus=       
-    i_minus=
-    for i in 1:sample_periods
-        FP_array[:,i]=FP[indices[i]:Int(indices[i]+T-1)]
+function average_values(ref_chan,rise_time,trig_indices,I_0) #already normalized
+    sample_length=minimum(diff(trig_indices)) #find the spacing between trigger points, and use the smallest one
+    i_plus=zeros(sample_length)       
+    i_minus=zeros(sample_length)
+
+    for i in rise_time:sample_length
+        i_plus[i]=ref_chan[i]+ref_chan[i+sample_length]
+        i_minus[i]=ref_chan[i]-ref_chan[i+sample_length]
     end
-    for i in 1:averages
-        ts=ts_range[i]
-        i_plus[:,i]=FP_array[ts,:].+FP_array[Int(ts+T/2),:]
-        i_minus[:,i]=FP_array[ts,:].-FP_array[Int(ts+T/2),:]
-        # sample_plus=range(ts,step=T,length=sample_periods) #defines evenly spaced samples. Need to correct for drift in the trigger.
-        # sample_minus=sample_plus.+Int(T/2)
-        # i_plus[:,i]=FP[sample_plus].+FP[sample_minus]
-        # i_minus[:,i]=FP[sample_plus].-FP[sample_minus]
-    end
-    i_plus=mean(i_plus,dims=2)
-    i_minus=mean(i_minus,dims=2)
-    return i_plus/I_0,i_minus/I_0
+    i_plus=mean(i_plus)
+    i_minus=mean(i_minus)
+    return i_plus/I_0,i_minus/I_0 #NB not exactly I_0 but accounts for the difference in the intensity over time
 end
 
-function calculate_L_L_prime_for_scan(sig_chan,ref_chan,trig_indices)
+function calculate_L_L_prime_for_scan(sig_chan,ref_chan,trig_indices,ref_trig_level_slope,ref_trig_level_intercept)
     trigger_level(x) = ref_trig_level_slope *x .+ ref_trig_level_intercept
     rise_time=7
     fall_time=2 #NB should be equivalent to be safe #the rise time of the intensity signal from the trigger point. total risetime = 2x risetime
     
-    L= Vector{Float64}(undef, length(trig_indices[1:2:end]))  
-    L_prime=Vector{Float64}(undef, length(trig_indices[1:2:end])) 
-    ts_range=
+    L= []
+    L_prime=[]
     
-    for (index,value) in enumerate(trig_indices[1:2:end]) #the start of every period
+    for i in 1:2:(length(trig_indices)-2) #the start of every period
         local_data=sig_chan[trig_indices[i]:trig_indices[i+2]]
-        I_0=trigger_level(period_number*data_points_per_period)
+        I_0=trigger_level(trig_indices[i+1])
         high_level=mean(ref_chan[trig_indices[i]+rise_time:trig_indices[i+1]-fall_time])
         low_level=mean(ref_chan[trig_indices[i+1]+fall_time:trig_indices[i+2]-rise_time])
-        Δim=high_level-low_level/(2) #I_0 =1
+        Δim=high_level-low_level/(2*I_0)
 
         #FP_modulation_sensitiviy=FP_shift/FP_modulation_amplitude#3.5e-9#meters/volt #measured calibration factor, not needed if measured_delta_lambda is available.
         # d_lambda_d_t=FP_shift/(1/(2*FP_frequency))#*1e12 #m #cycles/second  #NB could also be determined in terms of the min and max wavelength
         Δν_h=-3e10 #1/Δim*(-3e9)#(average_ν-ν_0)#<v>-vo/deltap/p  
         
-        i_plus, i_minus = average_values(local_data,rise_time,fall_time,trig_indices,index) #
-        L[index] = L0.(i_plus, i_minus, Δim) # Assuming Δim is available
-        L_prime[index] = L0_prime.(i_plus, i_minus, Δν_h, Δim) # Assuming Δν_h and Δim are available
+        i_plus, i_minus = average_values(local_data,rise_time,trig_indices[i:i+2],I_0) #
+        push!(L,L0(i_plus, i_minus, Δim))
+        push!(L_prime, L0_prime(i_plus, i_minus, Δν_h, Δim) )
     end
-
-
+    return L,L_prime
 end
 
 function prompt_for_integer()
@@ -172,7 +189,7 @@ ld_data=Dict("temp_setpoint"=>ld_data[:,1],"temp_sensor"=>ld_data[:,2],"time"=>l
 
 findmax(ld_data["temp_setpoint"])
 max_indices=findall(x->x==21,ld_data["temp_setpoint"])
-findmin(ld_data["temp_setpoint"])
+findmin(ld_data["temp_setpoint"]) 
 min_indices=findall(x->x==17.0,ld_data["temp_setpoint"])
 scan_points=ld_data["time"][alternate_vectors(min_indices,max_indices)] #get the time stamps for all the max and min points of the temperature scan. 
 
@@ -185,26 +202,16 @@ global const mod_rate=2e3
 global const data_points_per_period=sampling_rate/mod_rate
 global const data_points_per_half_period=round(Int,data_points_per_period/2)
 
-
-trig_indices=find_pulse_trig_points(ref_chan,ref_trig_level_intercept,ref_trig_level_slope)
+downsample=1000
+ref_trig_level_slope,ref_trig_level_intercept=find_ref_trigger_level(ref_chan,downsample)
+trig_indices=find_pulse_trig_points(ref_chan,ref_trig_level_intercept,ref_trig_level_slope,data_points_per_half_period)
 ref_chan,sig_chan,trig_indices=trim_channels(ref_chan,sig_chan,trig_indices)
 ref_chan,sig_chan=normalize_channels(ref_chan,sig_chan,trig_indices)
-downsample=100
-ref_trig_level_slope,ref_trig_level_intercept=find_ref_trigger_level(ref_chan,downsample)
+ref_trig_level_slope,ref_trig_level_intercept=find_ref_trigger_level(ref_chan,downsample)#calculate a new reference level for trimmed and normalized data
 
-calculate_L_L_prime_for_scan(ref_chan,sig_chan,trig_indices)
+L,L_prime=calculate_L_L_prime_for_scan(ref_chan,sig_chan,trig_indices,ref_trig_level_slope,ref_trig_level_intercept)
 
-
-# ref_chan=ref_chan[1:10000]
-# x_values = 1:length(ref_chan)
-# trigger_level = ref_trig_level_slope .* x_values .+ ref_trig_level_intercept
-# # Plotting
-# plot(x_values, ref_chan, label="Signal (ref_chan)", xlabel="Index", ylabel="Amplitude", title="Signal with Trigger Level and Trigger Points")
-# plot!(x_values, trigger_level, label="Trigger Level", color=:green, linewidth=2)
-# scatter!(trig_indices[1:40], ref_chan[trig_indices[1:40]], color=:red, marker=:circle, label="Trigger Points")
-
-# plot!(legend=:topright)
-# ans=find_pulse_trig_points(ref_chan,ref_trig_level_intercept,ref_trig_level_slope)
-# plot(ref_chan[trig_indices])
-# plot!(trigger_level[trig_indices])
-# plot(ref_chan[trig_indices].-trigger_level[trig_indices])
+_
+plot(L)
+plot(L_prime)
+plot(sig_chan[1:1000:end])
